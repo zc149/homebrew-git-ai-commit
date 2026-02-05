df7f0b4ee5c8fe4820dd8ae7c6b6139cbdef9287
kimjikwan <jikwan.kim@cj.net>
2026-02-05 18:02:19 +0900

    feat: Add Linux support

--- Formula/git-ai-commit.rb
+++ Formula/git-ai-commit.rb
@@ -9,11 +9,15 @@ class GitAiCommit < Formula
   elsif OS.mac? && Hardware::CPU.intel?
     url "https://github.com/zc149/git-ai-commit/releases/download/v1.0.0/git-ai-commit-darwin-amd64"
     sha256 "a8dcdddc15857dfd9a490b8904877059792c83e734595ebf372189c87bc939ae"
+  elsif OS.linux? && Hardware::CPU.intel?
+    url "https://github.com/zc149/git-ai-commit/releases/download/v1.0.0/git-ai-commit-linux-amd64"
+    sha256 "07faf3703450e1a6cc6d73c67d16d55ca155cc9897bfc41d8cd75343af40e69d"
   end
 
   def install
     bin.install "git-ai-commit-darwin-arm64" => "git-ai-commit" if OS.mac? && Hardware::CPU.arm?
     bin.install "git-ai-commit-darwin-amd64" => "git-ai-commit" if OS.mac? && Hardware::CPU.intel?
+    bin.install "git-ai-commit-linux-amd64" => "git-ai-commit" if OS.linux? && Hardware::CPU.intel?
   end
 
   test do
